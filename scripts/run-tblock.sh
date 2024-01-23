89ee1dfdbaecaceadd9f127d164de84c42081924
Woody Lam <woody.lam.cwl@gmail.com>
2024-01-23 22:02:19 +0000

    whoops fix

--- scripts/run-tblock.sh
+++ scripts/run-tblock.sh
@@ -7,4 +7,4 @@ script_dir=$( dirname "${BASH_SOURCE[0]}" )
 target_dir=$script_dir/../results/$1
 
 echo "Running tblock on path ${target_dir}"
-$script_dir/../tblock < $target_dir/delta-tblock.dat > $target_dir/../results/$1/tblock.log &
+$script_dir/../tblock < $target_dir/delta-tblock.dat > $target_dir/tblock.log &
