58d77cc88a0d07fe2ba7caedcfc3bf976d9e4b8e
Woody Lam <woody.lam.cwl@gmail.com>
2024-01-23 22:56:11 +0000

    deltagen

--- deltagen-ras224.f
+++ deltagen-ras224.f
@@ -47,7 +47,7 @@ C
       
       IF (RESULTS_EXIST) THEN
       	WRITE(6,*) 'Directory already exist: '//TRIM(OUTPUT_PATH)
-      	CALL EXIT(1)
+      	STOP 9
       ELSE
       	CALL system('mkdir -p '//TRIM(OUTPUT_PATH))
       ENDIF
