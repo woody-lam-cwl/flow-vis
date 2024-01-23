737fbd0616a0e310e0df4d0289148bdd1b088ba1
Woody Lam <woody.lam.cwl@gmail.com>
2024-01-23 21:56:16 +0000

    fix script

--- scripts/run-tblock.sh
+++ scripts/run-tblock.sh
@@ -1,7 +1,8 @@
 if [[ -z $1 ]]; then
     echo "No directory provided. No op."
+    exit 0
 fi
 
-script_dir = $(dirname "$[BASH_SOURCE[0]]")
+script_dir=$( dirname "$[BASH_SOURCE[0]]" )
 
 $script_dir/../tblock < $script_dir/../results/$1/delta-tblock.dat > $script_dir/../results/$1/tblock.log
