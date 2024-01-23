f8498546aca31fe11c795de041a7ad3f796ca4f6
Woody Lam <woody.lam.cwl@gmail.com>
2024-01-23 23:19:26 +0000

    add status script

---
+++ scripts/check_status.sh
@@ -0,0 +1,14 @@
+if [[ -z $1 ]]; then
+    echo "No directory provided. No op."
+    exit 0
+fi
+
+script_dir=$( dirname "${BASH_SOURCE[0]}" )
+target_dir=$script_dir/../results/$1
+
+cp $target_dir/tblock.log $script_dir/temp/tblock-$1.log
+cat $script_dir/temp/tblock-$1.log
+
+if [[ $2 -eq 1 ]]; then
+    rm -r $script_dir/temp/
+fi
