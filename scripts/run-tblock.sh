684f2c5bc33df5a14e3086f27fd7ffea72bee6d7
Woody Lam <woody.lam.cwl@gmail.com>
2024-01-23 22:01:31 +0000

    touch up

--- scripts/run-tblock.sh
+++ scripts/run-tblock.sh
@@ -4,5 +4,7 @@ if [[ -z $1 ]]; then
 fi
 
 script_dir=$( dirname "${BASH_SOURCE[0]}" )
+target_dir=$script_dir/../results/$1
 
-$script_dir/../tblock < $script_dir/../results/$1/delta-tblock.dat > $script_dir/../results/$1/tblock.log
+echo "Running tblock on path ${target_dir}"
+$script_dir/../tblock < $target_dir/delta-tblock.dat > $target_dir/../results/$1/tblock.log &
